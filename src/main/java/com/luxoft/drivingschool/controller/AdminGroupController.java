0fd1e3614f7a0fe30dd55d5f5b708c4208321dc0
nau <svetlana.lawrentyeva@gmail.com>
2015-03-05 22:38:41 +0200

    AdminGroupController.java: add static final constants

--- src/main/java/com/luxoft/drivingschool/controller/AdminGroupController.java
+++ src/main/java/com/luxoft/drivingschool/controller/AdminGroupController.java
@@ -15,6 +15,23 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class AdminGroupController {
 
+    private static final String VIEW_SEARCH_PATH = "/admin/group/search";
+    private static final String VIEW_SHOW_PATH = "admin/group/show";
+    private static final String VIEW_EDIT_PATH = "admin/group/edit";
+    private static final String REDIRECT_SHOW_TO_ID_PATH = "redirect:show?id=";
+
+    private static final String SEARCH_MAPPING_PATH="/search";
+    private static final String ADD_MAPPING_PATH="/add";
+    private static final String SAVE_MAPPING_PATH="/save";
+    private static final String SHOW_MAPPING_PATH="/show";
+    private static final String EDIT_MAPPING_PATH="/edit";
+
+    private static final String TEACHERS_ATTRIBUTE="teachers";
+    private static final String GROUPS_ATTRIBUTE="groups";
+    private static final String GROUP_ATTRIBUTE="group";
+
+    private static final String ID_REQUEST_PARAM="id";
+
     @Autowired
     TeacherService teacherService;
 
@@ -22,57 +39,57 @@ public class AdminGroupController {
     GroupService groupService;
 
     // Поиск
-    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    @RequestMapping(value = SEARCH_MAPPING_PATH, method = RequestMethod.GET)
     public String showAllGroups(Model model) {
 
-        model.addAttribute("groups", groupService.findAll());
-        model.addAttribute("teachers", teacherService.findAll());
-        return "admin/group/search";
+        model.addAttribute(GROUPS_ATTRIBUTE, groupService.findAll());
+        model.addAttribute(TEACHERS_ATTRIBUTE, teacherService.findAll());
+        return VIEW_SEARCH_PATH;
     }
 
     // Поиск по параметрам
-    @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public String findGroups(@RequestParam("teacherId") long teacherId, Model model) {
+    @RequestMapping(value = SEARCH_MAPPING_PATH, method = RequestMethod.POST)
+    public String findGroups(@RequestParam(ID_REQUEST_PARAM) long teacherId, Model model) {
 
-        model.addAttribute("groups", groupService.findByTeacherId(teacherId));
-        model.addAttribute("teachers", teacherService.findAll());
-        return "admin/group/search";
+        model.addAttribute(GROUPS_ATTRIBUTE, groupService.findByTeacherId(teacherId));
+        model.addAttribute(TEACHERS_ATTRIBUTE, teacherService.findAll());
+        return VIEW_SEARCH_PATH;
     }
 
     // Переход на форму сохранения
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    @RequestMapping(value = ADD_MAPPING_PATH, method = RequestMethod.GET)
     public String showRegistration(Model model) {
 
-        model.addAttribute("group", new Group());
-        model.addAttribute("teachers", teacherService.findAll());
+        model.addAttribute(GROUP_ATTRIBUTE, new Group());
+        model.addAttribute(TEACHERS_ATTRIBUTE, teacherService.findAll());
 
-        return "admin/group/edit";
+        return VIEW_EDIT_PATH;
     }
 
     // Сохранение и переход на форму просмотра
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String processRegistration(@ModelAttribute("group") Group group) {
+    @RequestMapping(value = SAVE_MAPPING_PATH, method = RequestMethod.POST)
+    public String processRegistration(@ModelAttribute(GROUP_ATTRIBUTE) Group group) {
 
         group = groupService.save(group);
-        return "redirect:show?id=" + group.getId(); // На страничку просмотра
+        return REDIRECT_SHOW_TO_ID_PATH + group.getId(); // На страничку просмотра
     }
 
     // Показ одной группы
-    @RequestMapping(value = "/show", method = RequestMethod.GET)
-    public String show(@RequestParam("id") long id, Model model) {
+    @RequestMapping(value = SHOW_MAPPING_PATH, method = RequestMethod.GET)
+    public String show(@RequestParam(ID_REQUEST_PARAM) long id, Model model) {
 
-        model.addAttribute("group", groupService.findOne(id));
-        return "admin/group/show";
+        model.addAttribute(GROUP_ATTRIBUTE, groupService.findOne(id));
+        return VIEW_SHOW_PATH;
     }
 
     // Редактирование одной группы
-    @RequestMapping(value = "/edit", method = RequestMethod.GET)
-    public String edit(@RequestParam("id") long id, Model model) {
+    @RequestMapping(value = EDIT_MAPPING_PATH, method = RequestMethod.GET)
+    public String edit(@RequestParam(ID_REQUEST_PARAM) long id, Model model) {
 
-        model.addAttribute("group", groupService.findOne(id));
-        model.addAttribute("teachers", teacherService.findAll());
+        model.addAttribute(GROUP_ATTRIBUTE, groupService.findOne(id));
+        model.addAttribute(TEACHERS_ATTRIBUTE, teacherService.findAll());
 
-        return "admin/group/edit";
+        return VIEW_EDIT_PATH;
     }
 
     @InitBinder
